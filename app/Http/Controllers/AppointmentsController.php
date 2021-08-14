0d01cab2505dd5bf4b0a853c7f4601e6548fea84
Sampan Verma <sampan.developer@gmail.com>
2021-08-14 16:38:37 +0530

    Added slot in past

--- app/Http/Controllers/AppointmentsController.php
+++ app/Http/Controllers/AppointmentsController.php
@@ -9,78 +9,83 @@ use Illuminate\Support\Facades\Validator;
 
 class AppointmentsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Event $event)
-    {
-        $appointments = $event->appointments()->get()->append(["date", "time"]);
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(Event $event)
+  {
+    $appointments = $event->appointments()->get()->append(["date", "time"]);
 
-        return $this->success($appointments);
+    return $this->success($appointments);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Event $event, Request $request)
+  {
+    $validator = Validator::make(
+      $request->all(),
+      [
+        'email' => 'required|email',
+        'first_name' => 'required|min:2',
+        'last_name' => 'required|min:2',
+        'slot_time' => 'required|date',
+      ]
+    );
+
+    if ($validator->fails()) {
+      return $this->fail($validator->errors());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Event $event, Request $request)
-    {
-        $validator = Validator::make($request->all(),
-            [
-                'email' => 'required|email',
-                'first_name' => 'required|min:2',
-                'last_name' => 'required|min:2',
-                'slot_time' => 'required|date',
-            ]
-        );
+    $validated = $validator->validated();
 
-        if ($validator->fails()) {
-            return $this->fail($validator->errors());
-        }
-
-        $validated = $validator->validated();
-
-        if (!$event->isSlotValid($validated["slot_time"])) {
-            return $this->fail("Time Slot is invalid.");
-        }
-
-        if (!$event->isSlotAvailable($validated["slot_time"])) {
-            return $this->fail("Time Slot is unavailable because it is fully booked.");
-        }
-
-        $appointment = new Appointment($validated);
-
-        $appointment->event_id = $event->id;
-
-        $appointment->save();
-
-        return $this->success($appointment, "Appointment was booked.");
+    if (!$event->isSlotValid($validated["slot_time"])) {
+      return $this->fail("Time Slot is invalid.");
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Appointment $appointment)
-    {
-        return $appointment;
+    if (!$event->isSlotAvailable($validated["slot_time"])) {
+      return $this->fail("Time Slot is unavailable because it is fully booked.");
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Appointment $appointment)
-    {
-        $appointment->delete();
-        return $this->success($appointment, "Appointment was cancelled.");
+    if (!$event->isSlotInPast($validated["slot_time"])) {
+      return $this->fail("Time Slot is in the past.");
     }
+
+    $appointment = new Appointment($validated);
+
+    $appointment->event_id = $event->id;
+
+    $appointment->save();
+
+    return $this->success($appointment, "Appointment was booked.");
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Appointment $appointment)
+  {
+    return $appointment;
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Appointment $appointment)
+  {
+    $appointment->delete();
+    return $this->success($appointment, "Appointment was cancelled.");
+  }
 }
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,154 +2,165 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
+// use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Cache;
 
 class Event extends Model
 {
-    use HasFactory;
-    protected $guarded = [];
-    public function cacheKey()
-    {
-        return sprintf(
-            "%s:%s:%s",
-            $this->getTable(),
-            $this->getKey(),
-            $this->updated_at->timestamp
-        );
-    }
-    public function appointments()
-    {
-        return $this->hasMany(Appointment::class, 'event_id');
-    }
+  use HasFactory;
+  protected $guarded = [];
+  public function cacheKey()
+  {
+    return sprintf(
+      "%s:%s:%s",
+      $this->getTable(),
+      $this->getKey(),
+      $this->updated_at->timestamp
+    );
+  }
+  public function appointments()
+  {
+    return $this->hasMany(Appointment::class, 'event_id');
+  }
 
-    public function getTimeSlots()
-    {
-        $timingStart = explode(":", $this->timing_start);
-        $timingEnd = explode(":", $this->timing_end);
-        $timingCurrent = $timingStart;
+  public function getTimeSlots()
+  {
+    $timingStart = explode(":", $this->timing_start);
+    $timingEnd = explode(":", $this->timing_end);
+    $timingCurrent = $timingStart;
 
-        $inactiveStart = explode(":", $this->inactive_start);
-        $inactiveEnd = explode(":", $this->inactive_end);
+    $inactiveStart = explode(":", $this->inactive_start);
+    $inactiveEnd = explode(":", $this->inactive_end);
 
-        $slots = [];
+    $slots = [];
 
-        while ($timingCurrent[0] <= $timingEnd[0]) {
-            if ($timingCurrent[0] == $timingEnd[0]) {
-                if ($timingCurrent[1] >= $timingEnd[1]) {
-                    break;
-                }
-            }
-            $slotName = implode(":", $timingCurrent);
-            if ($timingCurrent[0] >= $inactiveStart[0]) {
-                if ($timingCurrent[1] >= $inactiveStart[1]) {
-                    if ($timingCurrent[0] <= $inactiveEnd[0]) {
-                        if ($slotName < $this->inactive_end) {
-                            // Normalize the timestamps
-                            $timingCurrent[1] = $timingCurrent[1] + $this->length;
-                            if ($timingCurrent[1] >= 60) {
-                                $timingCurrent[0]++;
-                                $timingCurrent[1] -= 60;
-                            }
-                            if ($timingCurrent[0] < 10) {
-                                $timingCurrent[0] = "0" . intval($timingCurrent[0]);
-                            }
-                            if ($timingCurrent[1] < 10) {
-                                $timingCurrent[1] = "0" . intval($timingCurrent[1]);
-                            }
-                            continue;
-                        }
-                    }
-                }
-            }
-
-            $slots[$slotName] = 0;
-
-            // Normalize the timestamps
-            $timingCurrent[1] = $timingCurrent[1] + $this->length;
-            if ($timingCurrent[1] >= 60) {
+    while ($timingCurrent[0] <= $timingEnd[0]) {
+      if ($timingCurrent[0] == $timingEnd[0]) {
+        if ($timingCurrent[1] >= $timingEnd[1]) {
+          break;
+        }
+      }
+      $slotName = implode(":", $timingCurrent);
+      if ($timingCurrent[0] >= $inactiveStart[0]) {
+        if ($timingCurrent[1] >= $inactiveStart[1]) {
+          if ($timingCurrent[0] <= $inactiveEnd[0]) {
+            if ($slotName < $this->inactive_end) {
+              // Normalize the timestamps
+              $timingCurrent[1] = $timingCurrent[1] + $this->length;
+              if ($timingCurrent[1] >= 60) {
                 $timingCurrent[0]++;
                 $timingCurrent[1] -= 60;
-            }
-            if ($timingCurrent[0] < 10) {
+              }
+              if ($timingCurrent[0] < 10) {
                 $timingCurrent[0] = "0" . intval($timingCurrent[0]);
-            }
-            if ($timingCurrent[1] < 10) {
+              }
+              if ($timingCurrent[1] < 10) {
                 $timingCurrent[1] = "0" . intval($timingCurrent[1]);
+              }
+              continue;
             }
+          }
         }
+      }
 
-        return $slots;
+      $slots[$slotName] = 0;
+
+      // Normalize the timestamps
+      $timingCurrent[1] = $timingCurrent[1] + $this->length;
+      if ($timingCurrent[1] >= 60) {
+        $timingCurrent[0]++;
+        $timingCurrent[1] -= 60;
+      }
+      if ($timingCurrent[0] < 10) {
+        $timingCurrent[0] = "0" . intval($timingCurrent[0]);
+      }
+      if ($timingCurrent[1] < 10) {
+        $timingCurrent[1] = "0" . intval($timingCurrent[1]);
+      }
     }
 
-    public function getBookedSlotsAttribute()
-    {
-        return collect($this->getSlotsAttribute())->map(function ($day) {
-            return collect($day)->filter(function ($slot) {
-                return $slot != 0;
-            });
-        });
+    return $slots;
+  }
+
+  public function getBookedSlotsAttribute()
+  {
+    return collect($this->getSlotsAttribute())->map(function ($day) {
+      return collect($day)->filter(function ($slot) {
+        return $slot != 0;
+      });
+    });
+  }
+
+  public function getCachedBookedSlotsAttribute()
+  {
+    return Cache::remember($this->cacheKey() . ":slots", 60, function () {
+      return $this->getBookedSlotsAttribute();
+    });
+  }
+
+  public function getSlotsAttribute()
+  {
+    $activeDays = explode(",", $this->active_days);
+    $bookingStart = Carbon::parse($this->booking_start);
+    $bookingEnd = Carbon::parse($this->booking_end);
+    $bookingCurrent = $bookingStart;
+    $emptyTimeSlots = $this->getTimeSlots();
+    $days = [];
+    while ($bookingCurrent <= $bookingEnd) {
+
+      // Only the active days
+      if (!in_array($bookingCurrent->dayOfWeekIso, $activeDays)) {
+        $bookingCurrent->addDay();
+        continue;
+      }
+
+      $days[$bookingCurrent->format("Y-m-d")] = $emptyTimeSlots;
+      $bookingCurrent->addDay();
     }
 
-    public function getCachedBookedSlotsAttribute()
-    {
-        return Cache::remember($this->cacheKey() . ":slots", 60, function () {
-            return $this->getBookedSlotsAttribute();
-        });
+    $appointments = $this->appointments()->select("slot_time")->get();
+    foreach ($appointments as $appointment) {
+      // For all appointments subtract 1 from the slots
+      $appointmentTime = explode(" ", $appointment->slot_time);
+      $days[$appointmentTime[0]][$appointmentTime[1]] += 1;
     }
 
-    public function getSlotsAttribute()
-    {
-        $activeDays = explode(",", $this->active_days);
-        $bookingStart = Carbon::parse($this->booking_start);
-        $bookingEnd = Carbon::parse($this->booking_end);
-        $bookingCurrent = $bookingStart;
-        $emptyTimeSlots = $this->getTimeSlots();
-        $days = [];
-        while ($bookingCurrent <= $bookingEnd) {
+    return $days;
+  }
 
-            // Only the active days
-            if (!in_array($bookingCurrent->dayOfWeekIso, $activeDays)) {
-                $bookingCurrent->addDay();
-                continue;
-            }
-
-            $days[$bookingCurrent->format("Y-m-d")] = $emptyTimeSlots;
-            $bookingCurrent->addDay();
-        }
-
-        $appointments = $this->appointments()->select("slot_time")->get();
-        foreach ($appointments as $appointment) {
-            // For all appointments subtract 1 from the slots
-            $appointmentTime = explode(" ", $appointment->slot_time);
-            $days[$appointmentTime[0]][$appointmentTime[1]] += 1;
-        }
-
-        return $days;
-    }
-
-    public function isSlotValid($timeSlot)
-    {
-        $timeSlot = explode(" ", $timeSlot);
-        $slots = $this->getSlotsAttribute();
-        if (in_array($timeSlot[0], array_keys($slots))) {
-            if (in_array($timeSlot[1], array_keys($slots[$timeSlot[0]]))) {
-
-                return true;
-            }
-        }return false;
-    }
-
-    public function isSlotAvailable($timeSlot)
-    {
-        $appointmentCount = $this->appointments()->where("slot_time", $timeSlot)->count();
-
-        if ($appointmentCount >= $this->maximum_allowed) {
-            return false;
-        }
-
+  public function isSlotValid($timeSlot)
+  {
+    $timeSlot = explode(" ", $timeSlot);
+    $slots = $this->getSlotsAttribute();
+    if (in_array($timeSlot[0], array_keys($slots))) {
+      if (in_array($timeSlot[1], array_keys($slots[$timeSlot[0]]))) {
         return true;
+      }
     }
+    return false;
+  }
+
+  public function isSlotAvailable($timeSlot)
+  {
+    $appointmentCount = $this->appointments()->where("slot_time", $timeSlot)->count();
+
+    if ($appointmentCount >= $this->maximum_allowed) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public function isSlotInPast($timeSlot)
+  {
+    $currentTime = Carbon::now();
+    $timeSlot = Carbon::parse($timeSlot);
+    if ($timeSlot->lte($currentTime)) {
+      return false;
+    }
+    return true;
+  }
 }
